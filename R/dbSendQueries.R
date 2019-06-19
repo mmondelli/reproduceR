d54c0d2b0d2ca24ab2235451ec6734d59c7bc662
Malu Mondelli <mluiza@lncc.br>
2019-06-19 17:47:49 -0300

    new func

---
+++ R/dbSendQueries.R
@@ -0,0 +1,13 @@
+#' Functions to create the provenance database
+#'
+#' This contains the necessary functions to create the database file based on
+#' the model decribed on db_schema.sql
+#' @param con Connection to the provenance database
+#' @param file File with the schema definition (sql file)
+#' @keywords database, sql, creation
+#' @export
+
+dbSendQueries <- function(con,sql){
+  send <- function(sql,con){ DBI::dbSendQuery(con,sql) }
+  lapply(sql,send,con)
+}
