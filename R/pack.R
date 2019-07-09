89a7173a3e22002d08dd7e48cd59ae0e06cd923e
malumondelli <malumondelli@gmail.com>
2019-07-09 12:06:42 -0300

    fix copy vagrafile

--- R/pack.R
+++ R/pack.R
@@ -12,15 +12,15 @@ pack <- function(pack_project = '.'){
   if(!is.null(names(sessionInfo()$otherPkgs))){
     lapply(paste('package:',names(sessionInfo()$otherPkgs),sep=""), detach, character.only=TRUE, unload=TRUE)
   }
+  print("Copying default VagrantFile to the experiment directory")
+  #system(paste0('cp VagrantFile ', pack_project))
+  default_vagrant <- system.file('Vagrantfile', package='reproduceR')
+  system(paste0('cp ',default_vagrant, ' ', pack_project))
   print("Packing the project - this will take a few minutes.")
   library(packrat)
   packrat::init(project = pack_project, infer.dependencies = T, restart = F)
   print("Compressing project")
   packrat::bundle(overwrite = T)
-  print("Copying default VagrantFile to the experiment directory")
-  #system(paste0('cp VagrantFile ', pack_project))
-  default_vagrant <- system.file('Vagrantfile', package='reproduceR')
-  system(paste0('cp ',default_vagrant, ' ', pack_project))
   print("Vagrant up - this will take a few minutes")
   system(paste0('cd ',pack_project,'; vagrant up --provision'))
 }
